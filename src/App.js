13a691fb194fb839cdf68295d9a7e00e7c9cc5f9
RAJAT MEHRA <rajat.m.5278@gmail.com>
2020-12-12 23:41:30 +0530

    Added js files, screenshot

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./App.css"
+
+var place = {
+  "🚣": 'Person Rowing Boat',
+'🗾' : 'Map of Japan',
+'🌋' : 'Volcano',
+'🗻': 'Mount Fuji',
+'🏕️': 'Camping',
+'🏜️': 'Desert',
+'🏝️': 'Desert Island',
+'🏞️': 'National Park',
+'🏟️': 'Stadium',
+'🏛️': 'Classical Building',
+'🏗': 'Building Construction',
+'🛖': 'Hut',
+'🏘️': 'Houses'
+};
+
+let placeArray = Object.keys(place);
+
+export default function App() {
+  let [emoji, updateEmoji] = useState("");
+
+  function emojiHandler(emoji) {
+    updateEmoji(place[emoji])
+  }
+
+  return (
+    <div className="App">
+      <h1 className="header">Icon Interpreter</h1>
+      <h2 className="ans">{emoji}</h2>      
+      <h3 className="text">Select any place from below options</h3>
+      <div className="container">
+        {placeArray.map((key) => (
+          <span className="items" onClick={() => emojiHandler(key)}>
+            {key}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
